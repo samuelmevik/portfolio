8af49339c41b36d995947fa75bfe7697c865f0ad
samuelmevik <samuel.mevik@outlook.com>
2024-03-13 20:08:34 +0100

    theme toggler

---
+++ src/theme.ts
@@ -0,0 +1,32 @@
+type Theme = "light" | "dark";
+
+function getTheme(): Theme {
+  if (!localStorage.getItem("theme")) {
+    return matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+  return localStorage.getItem("theme") as Theme;
+}
+
+function setupTheme(element: HTMLButtonElement) {
+  element.addEventListener("click", () => {
+    setTheme(getTheme() === "light" ? "dark" : "light");
+  });
+  setTheme(getTheme());
+}
+
+function setTheme(theme: Theme) {
+  console.log("Setting theme to: " + theme);
+  switch (theme) {
+    case "light":
+      document.documentElement.classList.remove("dark");
+      break;
+    case "dark":
+      document.documentElement.classList.add("dark");
+      break;
+  }
+  localStorage.setItem("theme", theme);
+}
+
+export default setupTheme;
