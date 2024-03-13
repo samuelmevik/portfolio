ad9afbd40086318b4f75247a6f75eaea0ea351d9
samuelmevik <samuel.mevik@outlook.com>
2024-03-13 20:15:22 +0100

    fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Lora'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
