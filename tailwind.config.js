addfe53f474cd7954b492c7ac5aa3216005c5a5a
samuelmevik <samuel.mevik@outlook.com>
2024-03-13 20:57:37 +0100

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts}"],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          light: "#7dd3fc",
+          DEFAULT: "#0ea5e9",
+          dark: "#0369a1",
+        },
+      },
       fontFamily: {
         sans: ["'Lora'", ...defaultTheme.fontFamily.sans],
       },
