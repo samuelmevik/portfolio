21a88da3305058b38c14be273cd996e2bd56e477
samuelmevik <samuel.mevik@outlook.com>
2024-03-24 19:04:31 +0100

    Feat: Removed fetching Removed fetching due to the rate limit of the github api. Added a static avatar image instead.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,9 @@
-import useAvatar from "./js/avatar";
 import setupMobileMenu from "./js/mobile-menu";
 import setupTheme from "./js/theme";
-import { fetchRepo, fetchUser } from "./js/github";
-import useProjects from "./js/projects";
-
-const name = "samuelmevik";
 
 setupMobileMenu(
   document.querySelector<HTMLButtonElement>("#mobile-toggler")!,
-  document.querySelector<HTMLDivElement>("#mobile-menu")!
+  document.querySelector<HTMLDivElement>("#mobile-menu")!,
 );
 
 setupTheme(document.querySelector<HTMLButtonElement>("#theme-toggler")!);
-
-const user = await fetchUser(name);
-
-useAvatar(document.querySelector<HTMLImageElement>("#avatar")!, user);
-
-const repos = await fetchRepo(name);
-
-useProjects(document.querySelector<HTMLUListElement>("#projects")!, repos);
