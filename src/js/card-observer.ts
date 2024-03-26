589fe929e57151f4559e065182b6a0d18fe44318
samuelmevik <samuel.mevik@outlook.com>
2024-03-26 15:31:30 +0100

    Misc: animation direction Changed due to overflow issues

--- src/js/card-observer.ts
+++ src/js/card-observer.ts
@@ -2,12 +2,9 @@ const observer = new IntersectionObserver(
   (entries) => {
     for (const entry of entries) {
       entry.target.classList.toggle("show", entry.isIntersecting);
-      if (entry.isIntersecting) {
-        observer.unobserve(entry.target);
-      }
     }
   },
-  { rootMargin: "0px 0px -150px 0px" },
+  { rootMargin: "0px 0px -100px 0px" },
 );
 
 function setupCardObserver(elements: NodeListOf<HTMLElement>) {
